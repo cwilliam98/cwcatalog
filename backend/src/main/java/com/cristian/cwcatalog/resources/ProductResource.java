d451b6d7f6919f06da1f5f24aa0c4378079062a8
cwilliam98 <cristianwillian174@gmail.com>
2021-10-01 23:32:07 -0300

    Product service

--- backend/src/main/java/com/cristian/cwcatalog/resources/ProductResource.java
+++ backend/src/main/java/com/cristian/cwcatalog/resources/ProductResource.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cristian.cwcatalog.dto.ProductDTO;
@@ -15,25 +16,33 @@ import com.cristian.cwcatalog.services.ProductService;
 @RestController
 @RequestMapping(value = "/products")
 public class ProductResource {
-	
+
 	@Autowired
 	private ProductService service;
-	
+
 	@GetMapping
-	public ResponseEntity<List<ProductDTO>> findAll(){		
-		List<ProductDTO> list = service.findAll(); 
+	public ResponseEntity<List<ProductDTO>> findAll() {
+		List<ProductDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
-	
+
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<ProductDTO> findById(@PathVariable String id){
+	public ResponseEntity<ProductDTO> findById(@PathVariable String id) {
 		try {
 			ProductDTO product = service.findById(id);
 			return ResponseEntity.ok().body(product);
 		} catch (Exception e) {
 			return ResponseEntity.notFound().build();
 		}
-		
+	}
+
+	@GetMapping(value = "/search")
+	public ResponseEntity<List<ProductDTO>> search(
+			@RequestParam(value = "min_price", defaultValue = "") Double minPrice,
+			@RequestParam(value = "max_price", defaultValue = "") Double maxPrice,
+			@RequestParam(value = "q", defaultValue = "") String q) {
+		List<ProductDTO> product = service.search(q, minPrice, maxPrice);
+		return ResponseEntity.ok().body(product);
 	}
 
 }
--- backend/src/main/java/com/cristian/cwcatalog/services/ProductService.java
+++ backend/src/main/java/com/cristian/cwcatalog/services/ProductService.java
@@ -4,12 +4,16 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.cristian.cwcatalog.dto.ProductDTO;
 import com.cristian.cwcatalog.entities.Product;
+import com.cristian.cwcatalog.exceptions.ResourceNotFoundException;
 import com.cristian.cwcatalog.repositories.ProductRepository;
 
 
@@ -31,5 +35,47 @@ public class ProductService {
 		Product entity = obj.orElseThrow();
 		return new ProductDTO(entity);
 	}
-
+	
+	@Transactional(readOnly = true)
+	public List<ProductDTO> search(String query, Double minPrice, Double maxPrice){
+		return repository.findByNameAndDescriptionAndPrice(query, minPrice, maxPrice).stream().map(
+				x -> new ProductDTO(x)).collect(Collectors.toList());
+	}
+	
+	@Transactional
+	public ProductDTO insert(ProductDTO product){
+		Product entity = new Product();
+		entity.setName(product.getName());
+		entity.setDescription(product.getDescription());
+		entity.setPrice(product.getPrice());
+		entity = repository.save(entity);
+		return new ProductDTO(entity);
+	}
+	
+	@Transactional
+	public ProductDTO update(String id,ProductDTO dto) {
+		try {
+			Product entity = repository.getOne(id);
+			copyDtoToEntity(dto, entity);
+			entity = repository.save(entity);
+			return new ProductDTO(entity);
+		} catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found" + id);
+		}
+	}
+	
+	@Transactional
+	public void delete(String id) {
+		try {
+			repository.deleteById(id);
+		} catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+	}
+	
+	public void copyDtoToEntity(ProductDTO dto, Product entity) {
+		entity.setName(dto.getName());
+		entity.setDescription(dto.getDescription());
+		entity.setPrice(dto.getPrice());
+	}
 }
